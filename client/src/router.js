9a6e3bb7a1920079752523ae2076d4e54d939bd1
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-10-07 17:42:24 +0300

    added bootstrap

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Fetch from './components/fetch.vue';
+import Todos from './components/todos.vue';
 
 Vue.use(Router);
 
@@ -20,6 +22,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/fetch',
+      name: 'fetch',
+      component: Fetch,
+    },
+    {
+      path: '/todos',
+      name: 'todos',
+      component: Todos,
     },
   ],
 });
