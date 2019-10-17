9a4c66ffa2575c0eacf38e5f25dfb02d2ee48849
Shvd <2100992@gmail.com>
2019-10-17 09:17:03 +0300

    for sync

---
+++ client/src/components/changeTask.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <b-modal ref="addTodoModal" id="todo-modal" title="Изменить задачу" hide-footer>
+            <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+                <b-form-group
+                    id="form-description-group"
+                    label="Описание:"
+                    label-for="form-description-input"
+                >
+                    <b-form-input
+                        id="form-description-input"
+                        type="text"
+                        v-model="addTodoForm.description"
+                        required
+                        placeholder="Изменить задачу"
+                    ></b-form-input>
+                </b-form-group>
+                <b-form-group id="form-complete-group"></b-form-group>
+                <b-button type="submit" variant="primary">Изменить</b-button>
+                <b-button type="reset" variant="danger">Сброс</b-button>
+            </b-form>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            addTodoForm: {
+                description: ""
+            }
+            // todoListURL: "",
+            // userName: "",
+        };
+    },
+
+    props: ["todoListURL", "userName"],
+
+    methods: {
+        onSubmit(event) {
+            event.preventDefault();
+            this.$refs.addTodoModal.hide();
+            const requestData = {
+                description: this.addTodoForm.description
+            };
+            axios
+                .post(this.todoListURL + this.userName, requestData)
+                .then(() => {
+                    this.$emit("addedTask", this.addTodoForm.description);
+                    this.resetForm();
+                });
+        },
+        onReset(event) {
+            event.preventDefault();
+            this.$refs.addTodoModal.hide();
+            this.resetForm();
+        },
+        resetForm() {
+            this.addTodoForm.description = "";
+            // this.addTodoForm.is_completed = [];
+        }
+    }
+};
+</script>
--- client/src/components/todo_table.vue
+++ client/src/components/todo_table.vue
@@ -1,29 +1,61 @@
 <template>
-    <table class="table table-bordered">
-        <thead class>
-            <tr>
-                <th>№</th>
-                <th>Задача</th>
-                <th>Выполнить</th>
-                <th>Удалить</th>
-            </tr>
-        </thead>
+    <div>
+        <!-- модальное окно для изменения задачи 
+        !!! ToDo - вынести в отдельный компонент
+        ---------------------------------------------------------------------->
+        <b-modal ref="changeTodoModal" id="change-modal" title="Изменить задачу" hide-footer>
+            <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+                <b-form-group
+                    id="form-description-group"
+                    label="Описание:"
+                    label-for="form-description-input"
+                >
+                    <b-form-input
+                        id="form-description-input"
+                        type="text"
+                        v-model="addTodoForm.description"
+                        required
+                        placeholder="Изменить задачу"
+                    ></b-form-input>
+                </b-form-group>
+                <b-form-group id="form-complete-group"></b-form-group>
+                <b-button type="submit" variant="primary">Изменить</b-button>
+                <b-button type="reset" variant="danger">Сброс</b-button>
+            </b-form>
+        </b-modal>
+        <!-- таблица с задачами 
+        ---------------------------------------------------------------------->
+        <table class="table table-bordered">
+            <thead class>
+                <tr>
+                    <th>№</th>
+                    <th>Задача</th>
+                    <th>Выполнить</th>
+                    <th>Удалить</th>
+                </tr>
+            </thead>
 
-        <tbody>
-            <tr v-for="(todo, index) in todos" :key="index">
-                <td v-bind:class="todo.uid" class="todo-uid">{{ todo.id }}</td>
-                <td v-bind:class="todo.uid" v-on:dblclick="testAllert">{{ todo.desc }}</td>
-                <td v-bind:class="todo.uid" class="complTask">
-                    <b-form-checkbox v-bind:class="todo.uid" switch size="lg"></b-form-checkbox>
-                    <!-- <span v-if="todo.is_completed">Выполнено</span>
-                    <span v-else>Не выполнено</span>-->
-                </td>
-                <td v-bind:class="todo.uid" class="delTask">
-                    <button type="button" class="btn btn-danger btn-sm">X</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+            <tbody>
+                <tr v-for="(todo, index) in todos" :key="index">
+                    <td v-bind:class="todo.uid" class="todo-uid">{{ todo.id }}</td>
+                    <td
+                        v-bind:class="todo.uid"
+                        :id="todo.uid"
+                        v-on:dblclick="testAllert"
+                    >{{ todo.desc }}</td>
+                    <td v-bind:class="todo.uid" class="complTask">
+                        <b-form-checkbox v-bind:class="todo.uid" switch size="lg"></b-form-checkbox>
+                        <!-- <span v-if="todo.is_completed">Выполнено</span>
+                        <span v-else>Не выполнено</span>-->
+                    </td>
+                    <td v-bind:class="todo.uid" class="delTask">
+                        <button type="button" class="btn btn-danger btn-sm">X</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- -------------------------------------------------------------------- -->
+    </div>
 </template>
 
 <style scoped>
@@ -42,12 +74,18 @@ td {
 </style>
 
 <script>
+// import changeTask from "./changeTask.vue";
+
 export default {
     props: ["todos"],
     methods: {
-        testAllert() {
-            alert("click");
+        testAllert(enent) {
+            console.log(event.target.id);
+            this.$refs.changeTodoModal.show();
         }
+    },
+    components: {
+        // changetask: changeTask
     }
 };
 </script>
