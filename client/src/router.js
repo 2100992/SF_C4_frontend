427e645e0c39da2296fe39c7f90981264006056a
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-10-21 18:22:06 +0300

    removed unnecessary files

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './components/Home.vue';
-import Fetch from './components/fetch.vue';
-import Todos from './components/todos.vue';
-import TodosLS from './components/todos_LS.vue';
+// import Fetch from './components/fetch.vue';
+// import Todos from './components/todos.vue';
+// import TodosLS from './components/todos_LS.vue';
 
 Vue.use(Router);
 
@@ -24,20 +24,20 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
-    {
-      path: '/fetch',
-      name: 'fetch',
-      component: Fetch,
-    },
-    {
-      path: '/todos',
-      name: 'todos',
-      component: Todos,
-    },
-    {
-      path: '/todos_LS',
-      name: 'todosLS',
-      component: TodosLS,
-    },
+    // {
+    //   path: '/fetch',
+    //   name: 'fetch',
+    //   component: Fetch,
+    // },
+    // {
+    //   path: '/todos',
+    //   name: 'todos',
+    //   component: Todos,
+    // },
+    // {
+    //   path: '/todos_LS',
+    //   name: 'todosLS',
+    //   component: TodosLS,
+    // },
   ],
 });
