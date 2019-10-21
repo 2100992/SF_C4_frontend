4f05953ccda99b1046babc1864d8aa7f9c2a18a9
Швецов Сергей Владимирович <shvetsov@elvees.com>
2019-10-21 18:22:31 +0300

    add about info

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <h1>Frontand часть домашнего задания по модулю C4</h1>
+      <p>Надеюсь интерфейс достаточно интуитивно понятен</p>
+      <p>Изменения описания задачи происходит по двойному клику.</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+h1,
+h2,
+h3,
+p {
+  text-align: left;
+}
+</style>
